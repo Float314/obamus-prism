95a11d400e19dcb2151c9d53ed6765538df17294
Cyphrixz <jangamshourya507@gmail.com>
2026-03-10 23:08:04 +0530

    Added sounds and fixed some things :/

--- src/popup_fixed.cpp
+++ src/popup_fixed.cpp
@@ -1,11 +1,14 @@
 #include <Geode/Geode.hpp>
 #include <Geode/ui/Popup.hpp>
+#include <Geode/fmod/fmod.h>
 
 using namespace geode::prelude;
 
 // this is the updated code
 
 class MyPopup : public geode::Popup {
+protected:
+    CCSprite* obamaSpr;
 public:
     static MyPopup* create() {
         auto ret = new MyPopup();
@@ -16,14 +19,24 @@ public:
         delete ret;
         return nullptr;
     }
-
+    // 135169432
     bool init() override {
         if (!Popup::init(380.f, 250.f)) return false;
         
-        auto obamaSpr = CCSprite::create("obama.png"_spr);
-        if (obamaSpr) {
+        obamaSpr = CCSprite::create("obama.png"_spr);
+        /* if (obamaSpr) {
             m_mainLayer->addChildAtPosition(obamaSpr, geode::Anchor::Center);
-        }
+        } */ 
+
+        auto obamaBtn = CCMenuItemSpriteExtra::create(
+            obamaSpr,
+            this,
+            menu_selector(MyPopup::onObamaBtn)
+        );
+        // m_mainLayer->addChild(obamaBtn);
+        m_buttonMenu->addChild(obamaBtn);
+        obamaBtn->setPosition({190.f, 125.f});
+
 
         auto label = CCLabelBMFont::create("Barrack Obama :)", "bigFont.fnt");
         if (label) {
@@ -32,5 +45,21 @@ public:
         } else return false;
 
         return true;
+    }
+
+    void onObamaBtn(CCObject* sender) { 
+        obamaSpr->runAction(CCRotateBy::create(1.f, 360.f));
+
+        auto system = FMODAudioEngine::get()->m_system;
+        FMOD::Sound* sound;
+        FMOD::Channel* channel;
+
+        system->createSound(
+            (string::pathToString(Mod::get()->getResourcesDir()/"ohd.ogg")).c_str(),
+            FMOD_DEFAULT, nullptr, &sound
+        );
+        system->playSound(sound, nullptr, false, &channel);
+        channel->setVolume(FMODAudioEngine::get()->m_sfxVolume);
+
     }
 };
